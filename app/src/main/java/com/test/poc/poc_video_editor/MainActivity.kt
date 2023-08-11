68cc65ea9b99217c6f922af8b9eace4608d4219c
Jaison Macklin Menezes <macklin.jaison@gmail.com>
2023-08-11 10:37:28 +0530

    Initial commit

---
+++ app/src/main/java/com/test/poc/poc_video_editor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.poc.poc_video_editor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
