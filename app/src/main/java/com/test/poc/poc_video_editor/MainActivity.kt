231f99b9ce33f523d7603aeadf19ebb268f76e6b
Jaison Macklin Menezes <macklin.jaison@gmail.com>
2023-08-11 11:03:15 +0530

    Initialized data-binding

--- app/src/main/java/com/test/poc/poc_video_editor/MainActivity.kt
+++ app/src/main/java/com/test/poc/poc_video_editor/MainActivity.kt
@@ -2,10 +2,22 @@ package com.test.poc.poc_video_editor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import com.test.poc.poc_video_editor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // data-binding
+        val layoutInflater = LayoutInflater.from(this)
+        binding=DataBindingUtil.inflate(layoutInflater,R.layout.activity_main,null,false)
+        // TODO : INIT
+
+       setContentView(binding.root)
     }
 }
